47b0666cd195b518559f61731718e8799dfe823f
Líverton Oliveira <lifelipe7@live.com>
2017-04-16 15:18:53 -0300

    first commit

---
+++ config/env/development.js
@@ -0,0 +1,9 @@
+module.exports = {
+  db: 'mongodb://localhost/mean-book',
+  sessionSecret: 'developmentSessionSecret',
+  facebook: {
+    clientID: '1010808938964760',
+    clientSecret: 'e736b512211c5610dfc63b37081fdda3',
+    callbackURL: 'http://localhost:3000/oauth/facebook/callback'
+  }
+};
---
+++ config/env/test.js
@@ -0,0 +1,10 @@
+module.exports = {
+  db: 'mongodb://localhost/mean-book-test',
+  sessionSecret: 'Your Application Session Secret',
+  viewEngine: 'ejs',
+  facebook: {
+    clientID: '1010808938964760',
+    clientSecret: 'e736b512211c5610dfc63b37081fdda3',
+    callbackURL: 'http://localhost:3000/oauth/facebook/callback'
+  }
+}
